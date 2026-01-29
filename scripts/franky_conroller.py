ea429d0ebe88bae9b8ddfcd3642068f9d726a8e7
Zi Tao Li <dylanlzt2003@gmail.com>
2026-01-29 00:20:34 -0800

    update: fixed errors on franky_controller.py

--- scripts/franky_conroller.py
+++ scripts/franky_conroller.py
@@ -1,16 +1,18 @@
 from franky import Affine, CartesianMotion, Robot, ReferenceType, Gripper
 import rclpy
-import rclpy.node import Node
+from rclpy.node import Node
 import lerobot_teleoperator_violin as violin_mod
 from lerobot.teleoperators.config import TeleoperatorConfig
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
 from scipy.spatial.transform import Rotation
+import numpy as np
 
 ROBOT_IP = "192.168.137.2"
 
+
 class FrankyListener(Node):
-    def __init__(self, controller: FrankyController):
+    def __init__(self, controller):
         super().__init__("franky_listener")
         self.pose_sub = self.create_subscription(
             PoseStamped,
@@ -27,8 +29,8 @@ class FrankyListener(Node):
         self.controller = controller
 
     def eef_cb(self, msg: PoseStamped):
-        # initialize initial eef pose only if leader arm is connected
-        if self.controller.initial_eef_pose is None and self.controller.leader_connected():
+        if self.controller.initial_eef_pose is None and self.controller.leader_connected:
+            # initialize initial eef pose only if leader arm is connected
             self.controller.initial_eef_pose = [
                 msg.pose.position.x,
                 msg.pose.position.y,
@@ -39,8 +41,8 @@ class FrankyListener(Node):
                 msg.pose.orientation.w,
             ]
             self.get_logger().info(
-                "Initial EEF pose:  [%s, %s, %s], [%s, %s, %s, %s]", 
-                msg.pose.position.x, msg.pose.position.y, msg.pose.position.z, 
+                "Initial EEF pose:  [%s, %s, %s], [%s, %s, %s, %s]",
+                msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
                 msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w
             )
             self.controller.initial_q_inverse = Rotation.from_quat([
@@ -48,15 +50,16 @@ class FrankyListener(Node):
                 msg.pose.orientation.y,
                 msg.pose.orientation.z,
                 msg.pose.orientation.w,
-            ]).inv().tolist()
-
-        # self.get_logger().info(f"heard: linear x={msg.linear.x:.3f}, z={msg.angular.z:.3f}")
-        if self.controler.initial_eef_pose is not None and self.controller.leader_connected():
-            self.controller.get_current_pose(msg)
+            ]).inv()
+        elif self.controller.initial_eef_pose is not None and self.controller.leader_connected:
+            # update current eef pose to controller
+            self.controller.update_current_pose(msg)
 
     def gripper_cb(self, msg: JointState):
-        # self.get_logger().info(f"heard: linear x={msg.linear.x:.3f}, z={msg.angular.z:.3f}")
-        pass
+        if self.controller.leader_connected:
+            width = msg.position[0]
+            self.controller.update_gripper(width)
+
 
 class FrankyController:
     def __init__(self):
@@ -71,59 +74,62 @@ class FrankyController:
         self.gripper_force = 20.0
 
         # leader
-        self.leader = self.leader_init()
+        self.leader = self._leader_init()
+        self.leader_connected = False
 
         self.initial_eef_pose = None
         self.initial_q_inverse = None
-    
-    def leader_init(self):
+
+    def _leader_init(self):
         cfg_cls = None
         for obj in vars(violin_mod).values():
             if isinstance(obj, type) and issubclass(obj, TeleoperatorConfig) and obj is not TeleoperatorConfig:
                 cfg_cls = obj
                 break
         assert cfg_cls is not None, "Could not find a TeleoperatorConfig in lerobot_teleoperator_violin"
-        
+
         cfg = cfg_cls(port="/dev/ttyUSB1", id="my_awesome_staraiviolin_arm")
         teleop = cfg_cls.__name__.removesuffix("Config")
         teleop_cls = getattr(violin_mod, teleop)
         self.leader = teleop_cls(cfg)
 
-    def move_arm(self, translation: list, rotation: list):
-        # check this code for with quat rotation and reference type
-        motion = CartesianMotion(Affine(translation, rotation), ReferenceType.Relative)
-        # robot.move(motion)
-
-    def move_gripper(self, width: float, speed = 0.02):
-        # move the fingers to a specific width
-        success = self.gripper.move(width, speed)
-
-    def get_current_pose(self, msg: PoseStamped):
-            # calculate the translation and rotation relative to the initial pose
-            translation_rel = [
-                msg.pose.position.x - self.initial_eef_pose[0],
-                msg.pose.position.y - self.initial_eef_pose[1],
-                msg.pose.position.z - self.initial_eef_pose[2],
-            ]
-            q_current = [
-                msg.pose.orientation.x,
-                msg.pose.orientation.y,
-                msg.pose.orientation.z,
-                msg.pose.orientation.w,
-            ]
-            # relative rotation is q_rel = q_current * q_initial_inverse
-            rotation_rel = Rotation.from_quat(q_current) * Rotation.from_quat(self.initial_q_inverse)
-            self.move_arm(translation_rel, rotation_rel.tolist())
-
-    def leader_connected(self) -> bool:
-        # check if leader arm is connected
         try:
             self.leader.connect()
             print("[INFO] Leader arm connected successfully.", flush=True)
-            return True
+            self.leader_connected = True
         except Exception as exc:
             print(f"[WARN] Leader arm not connected; publishing zeros. Error: {exc}", flush=True)
-            return False
+
+    def _move_arm(self, translation: list, rotation: list):
+        motion = CartesianMotion(Affine(translation, rotation), ReferenceType.Relative)
+        # self.robot.move(motion)
+
+    def _move_gripper(self, width: float, speed = 0.02):
+        # move the fingers to a specific width
+        # success = self.gripper.move(width, speed)
+        pass
+
+    def update_current_pose(self, msg: PoseStamped):
+        # calculate the translation and rotation relative to the initial pose
+        dx = msg.pose.position.x - self.initial_eef_pose[0]
+        dy = msg.pose.position.y - self.initial_eef_pose[1]
+        dz = msg.pose.position.z - self.initial_eef_pose[2]
+        translation_rel = np.array([[dx], [dy], [dz]], dtype=np.float64)
+
+        q_current = [
+            msg.pose.orientation.x,
+            msg.pose.orientation.y,
+            msg.pose.orientation.z,
+            msg.pose.orientation.w,
+        ]
+        # relative rotation is q_rel = q_current * q_initial_inverse
+        q_rel = (Rotation.from_quat(q_current) * self.initial_q_inverse).as_quat()
+        rotation_rel = q_rel.reshape(4, 1).astype(np.float64)
+
+        self._move_arm(translation_rel, rotation_rel)
+
+    def update_gripper(self, width: float):
+        self._move_gripper(width, self.gripper_speed)
 
 
 def main():
@@ -133,6 +139,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == "__main__":
     main()
